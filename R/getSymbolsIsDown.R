0d38c7bec0b686c6e77916f06caed36cedfe48c8
OMurilloF <104952658+OMurilloF@users.noreply.github.com>
2022-05-04 23:33:28 -0500

    Update getSymbolsIsDown.R

--- R/getSymbolsIsDown.R
+++ R/getSymbolsIsDown.R
@@ -1,4 +1,4 @@
-getSymbolsIsDown <- function(tickerList,fromDate="1970-01-01",toDate=Sys.Date(),interval="Daily",events="Historical") {
+getSymbolsIsDown <- function(tickerList,fromDate="1970-01-01",toDate=Sys.Date(),interval="Daily",events="Historical",specificName="False") {
 
   if (!require(xts)) {
 
@@ -31,15 +31,34 @@ getSymbolsIsDown <- function(tickerList,fromDate="1970-01-01",toDate=Sys.Date(),
       stop("No event set. Please input either Historical, Dividends, Splits or Capital parameter.")
     }
 
-    for (i in tickerList) {
-      myURL <- paste0("https://query1.finance.yahoo.com/v7/finance/download/",i,"?period1=",fromDate,"&period2=",toDate,"&interval=",interval,"&events=",events,"&includeAdjustedClose=true")
-      X_Manual <- read.csv(myURL)
-      X_Manual <- xts(X_Manual[, -1], order.by=as.Date(X_Manual$Date))
-      myDim <- dim(X_Manual)[2]
-      for (x in 1:myDim) {
-        colnames(X_Manual)[x]<-paste0(i,".",colnames(X_Manual[,x]))
+    if (specificName=="False" | specificName=="false" | specificName=="FALSE") {
+
+      for (i in tickerList) {
+        myURL <- paste0("https://query1.finance.yahoo.com/v7/finance/download/",i,"?period1=",fromDate,"&period2=",toDate,"&interval=",interval,"&events=",events,"&includeAdjustedClose=true")
+        X_Manual <- read.csv(myURL)
+        X_Manual <- xts(X_Manual[, -1], order.by=as.Date(X_Manual$Date))
+        myDim <- dim(X_Manual)[2]
+        for (x in 1:myDim) {
+          colnames(X_Manual)[x]<-paste0(i,".",colnames(X_Manual[,x]))
+        }
+        assign(i, X_Manual, envir = .GlobalEnv)
       }
-      assign(i, X_Manual, envir = .GlobalEnv)
+
+    } else if (specificName=="True" | specificName=="true" | specificName=="TRUE") {
+
+      for (i in tickerList) {
+        myURL <- paste0("https://query1.finance.yahoo.com/v7/finance/download/",i,"?period1=",fromDate,"&period2=",toDate,"&interval=",interval,"&events=",events,"&includeAdjustedClose=true")
+        X_Manual <- read.csv(myURL)
+        X_Manual <- xts(X_Manual[, -1], order.by=as.Date(X_Manual$Date))
+        myDim <- dim(X_Manual)[2]
+        for (x in 1:myDim) {
+          colnames(X_Manual)[x]<-paste0(i,".",colnames(X_Manual[,x]))
+        }
+        assign(paste0(i,"_",interval,"_",events), X_Manual, envir = .GlobalEnv)
+      }
+
+    } else {
+      stop("No specificName argument set. Please input either True or False.")
     }
 
   }
