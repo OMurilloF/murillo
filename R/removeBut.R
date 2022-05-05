f30020b844d8ea42438a1d051bc690abd44c2cfb
OMurilloF <104952658+OMurilloF@users.noreply.github.com>
2022-05-04 21:57:59 -0500

    Update removeBut.R

--- R/removeBut.R
+++ R/removeBut.R
@@ -1,3 +1,12 @@
+#' Add together two numbers
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
 removeBut <- function(listToNotRemove) {
   rm(list = ls(pos = ".GlobalEnv")[!ls(pos = ".GlobalEnv") %in% listToNotRemove], pos = ".GlobalEnv")
 }
